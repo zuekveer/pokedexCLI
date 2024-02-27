a99c88ec81e67034dd1ebee0746cf04c0b8f73a2
Artem Valid <simetramonk@gmail.com>
2024-02-27 20:07:52 +0300

    Commit_2: update.

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"time"
+
+	"github.com/zuekveer/courses/projects/pokedexcli/internal/pokeapi"
+	"github.com/zuekveer/pokedexCLI/internal/pokeapi"
+)
+
 func main() {
-	startRepl()
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)
 }
