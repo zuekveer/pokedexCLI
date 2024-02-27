9ce1eb48737b73d1e6a9209b696675551ec95a2b
Artem Valid <simetramonk@gmail.com>
2024-02-28 01:33:37 +0300

    Commit_9: update.

--- repl.go
+++ repl.go
@@ -71,6 +71,11 @@ func getCommands() map[string]cliCommand {
 			description: "Attempt to catch a pokemon",
 			callback:    commandCatch,
 		},
+		"inspect": {
+			name:        "inspect <pokemon_name>",
+			description: "View details about a caught Pokemon",
+			callback:    commandInspect,
+		},
 		"explore": {
 			name:        "explore <location_name>",
 			description: "Explore a location",
